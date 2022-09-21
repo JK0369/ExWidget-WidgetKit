88cd4e4b30f8c86a6839b00e73deb1c3c0e71bb3
jake <palatable7@naver.com>
2022-09-22 00:31:37 +0900

    Initial Commit

---
+++ ExWidget/ExWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExWidgetApp.swift
+//  ExWidget
+//
+//  Created by 김종권 on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ExWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
